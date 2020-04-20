ea3e2a8ca7633f61083ccbcb94fda16f8c408fc1
arcadia2947 <arcadia@DESKTOP-P4FRCR3.localdomain>
2020-04-20 12:19:13 +0200

    more test

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -16,12 +16,12 @@ int	ft_putstr_fd(char *s, int fd, int max, int p)
 {
 	int	i;
 
-	if (s == NULL && param->p == 0)
+	if (s == NULL && p == 0)
 	{
 		ft_putstr_fd("(null)", 1, 0, 0);
 		return (6);
 	}
-	if (s == NULL && param->p == 1)
+	if (s == NULL && p == 1)
 		return (0);
 	i = ft_strlen(s);
 	if (ft_strlen(s) > max && p == 1)
